8ea71dcb41d3df58080967ad76f589e3337160aa
erik pantzar <erik@epantzar.se>
2021-08-09 00:31:26 +0200

    Added variables and typo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import './styles/_var.css'
+import './styles/base.css'
 
 ReactDOM.render(
   <React.StrictMode>
