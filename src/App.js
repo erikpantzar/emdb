85c7a4424dd6946c8d9abf681c7553ce5429c85d
Erik Pantzar <erik@epantzar.se>
2024-04-11 18:53:01 +0200

    Trending with blending movies and Tv

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import ScrollToTop from './ScrollToTop'
-import Nav from './components/Nav/Nav'
-
-import Movie from './Views/Movie'
-import Home from './Views/Home'
-import Discover from './Views/Discover'
-import Actor from './Views/Actor'
-import About from './Views/About'
-import Search from './components/Search/Search'
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Trending from './components/Trending/Trending'
 
 export default function App() {
-  const [searchVisible, setSearchVisible] = useState(false)
-
   return (
     <Router>
-      <ScrollToTop>
-        {searchVisible && <Search toggleSearch={setSearchVisible} />}
-
-        <Nav />
-
-        <button onClick={() => setSearchVisible(!searchVisible)} type="button">
-          Search
-        </button>
-
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          <Route exact path="/discover">
-            <Discover />
-          </Route>
-
-          <Route exact path="/about" component={About} />
-
-          <Route exact path={`/movie/:id`} component={Movie} />
-          <Route exact path={`/actor/:personId`} component={Actor} />
-        </Switch>
-      </ScrollToTop>
+      <Trending></Trending>
     </Router>
   )
 }
--- src/components/Trending/Trending.js
+++ src/components/Trending/Trending.js
@@ -1,75 +1,166 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import './Trending.css'
 
-import api from '../../api'
-import MovieList from '../MovieList/MovieList'
+const API_KEY = process.env.REACT_APP_API_KEY
 
-const Trending = () => {
-  const [movies, setMovies] = useState([])
-  const [page, setPage] = useState(1)
-  const [maxPage, setMaxPage] = useState()
-
-  useEffect(() => {
-    async function fetch() {
-      if (page > maxPage) {
-        return false
-      }
-
-      try {
-        const res = await api.trending({ page: page })
-        setMovies([...movies, ...res.results])
-        setMaxPage(res.total_pages)
-      } catch (error) {
-        console.error(error)
-      }
-    }
-
-    fetch()
-  }, [page])
-
-  const handleScroll = (event) => {
-    const element = document.querySelector('#one')
-    const win = window.innerHeight
-    const bottom = element.getBoundingClientRect().bottom
-
-    if (bottom <= win) {
-      if (page < maxPage + 1) {
-        setPage(page + 1)
-      }
+// general helper
+// put in util ?
+const matchGenre = (id, genres) => {
+  for (let genre of genres) {
+    if (id === genre.id) {
+      return genre.name
     }
   }
 
+  return null
+}
+
+export default () => {
+  const [loading, setLoading] = useState(false)
+  const [trending, setTrending] = useState([])
+  const [movieGenres, setMovieGenre] = useState(null)
+  const [tvGenres, setTvGenre] = useState(null)
+
   useEffect(() => {
-    function list() {
-      document.addEventListener('scroll', handleScroll)
+    async function fetchData() {
+      const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`
+
+      try {
+        setLoading(true)
+
+        const res = await (
+          await fetch(url, {
+            headers: {
+              accept: 'application/json',
+            },
+          })
+        ).json()
+
+        setLoading(false)
+
+        setTrending(res.results)
+      } catch (err) {
+        console.error(err)
+        setInterval([])
+      }
     }
 
-    list()
+    async function listMoviesData() {
+      const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
 
-    return function cleanup() {
-      document.removeEventListener('scroll', handleScroll)
+      try {
+        const res = await (
+          await fetch(url, {
+            headers: {
+              accept: 'application/json',
+            },
+          })
+        ).json()
+
+        setMovieGenre(res)
+      } catch (err) {
+        console.error(err)
+      }
     }
-  })
 
-  const isMore = page < maxPage
+    async function listTvData() {
+      const url = `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`
+
+      try {
+        const res = await (
+          await fetch(url, {
+            headers: {
+              accept: 'application/json',
+            },
+          })
+        ).json()
+
+        setTvGenre(res)
+      } catch (err) {
+        console.error(err)
+        setTvGenre(null)
+      }
+    }
+
+    fetchData()
+    listMoviesData()
+    listTvData()
+  }, [])
 
   return (
-    <section id="one">
-      <h1>Trending</h1>
+    <div>
+      <h2>Trending media</h2>
 
-      <MovieList movies={movies} />
+      {loading && '<h3>Loading</h3>'}
 
-      {isMore && (
-        <button
-          type="button"
-          onClick={() => {
-            setPage(page + 1)
-          }}
-        >
-          Get more
-        </button>
+      {tvGenres && movieGenres && trending && trending.length > 0 && (
+        <section className="trending-list">
+          {trending.map((trendItem, idx) => {
+            if (trendItem.media_type === 'tv') {
+              return (
+                <TvCard
+                  key={idx}
+                  item={trendItem}
+                  genres={tvGenres.genres}
+                ></TvCard>
+              )
+            } else {
+              return (
+                <MovieCard
+                  key={idx}
+                  item={trendItem}
+                  genres={movieGenres.genres}
+                ></MovieCard>
+              )
+            }
+          })}
+        </section>
       )}
-    </section>
+    </div>
   )
 }
 
-export default Trending
+const thumbUrl = (id) => `https://image.tmdb.org/t/p/w300/${id}`
+
+const TvCard = ({ item, genres }) => (
+  <article className="card card-tv" key={item.id}>
+    <div className="card-media-type">TV</div>
+
+    <header>
+      <h2 className="card-title">{item.name}</h2>
+      {item.release_date && <span>Released: {item.release_date}</span>}
+      <p>{item.overview}</p>
+
+      <div className="genres">
+        {item.genre_ids.map((genreId) => (
+          <div className="card-tag genre">{matchGenre(genreId, genres)}</div>
+        ))}
+      </div>
+    </header>
+
+    <Thumbnail poster={item.poster_path} />
+  </article>
+)
+
+const MovieCard = ({ item, genres }) => (
+  <article className="card card-movie" key={item.id}>
+    <div className="card-media-type">Movie</div>
+
+    <header>
+      <h2 className="card-title">{item.title}</h2>
+      {item.release_date && <span>Released: {item.release_date}</span>}
+      <p>{item.overview}</p>
+
+      <div className="genres">
+        {item.genre_ids.map((genreId) => (
+          <div className="card-tag genre">{matchGenre(genreId, genres)}</div>
+        ))}
+      </div>
+    </header>
+    <Thumbnail poster={item.poster_path} />
+  </article>
+)
+
+const Thumbnail = ({ poster }) => {
+  return <img src={thumbUrl(poster)} alt="placeholder text" />
+}
